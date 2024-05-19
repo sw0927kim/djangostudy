680bda07be16c9dc6b4984697d01dcad62f678b2
Seongwon <tjddnslws@gmail.com>
2024-05-19 22:44:42 +0900

    사진 왜 안먹냐 css버근가

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,7 +5,7 @@ from .models import Choice, Question
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.views import generic
-
+from django.utils import timezone
 
 
 # def index(request):
@@ -33,8 +33,15 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        # """Return the last five published questions."""
+        # return Question.objects.order_by("-pub_date")[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+            :5
+        ]
 
 # def detail(request, question_id):
     # return HttpResponse("You're looking at question %s." % question_id)
@@ -50,6 +57,11 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 # def results(request, question_id):
@@ -61,7 +73,7 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
-    
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
